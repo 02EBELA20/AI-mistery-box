d319c90af4b8eb87a9f91d5908f71aaa6d4e9f69
levani <ebelashvililevani@yahoo.com>
2025-02-01 21:47:36 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true, // ✅ გამოსახულების ოპტიმიზაციის გამორთვა
-  }
+  output: "export", // ✅ ეს არის მნიშვნელოვანი Netlify-სთვის
 };
 
 export default nextConfig;
