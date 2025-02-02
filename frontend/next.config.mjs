21307388a47f2503d9196ddac2711d466d683f09
levani <ebelashvililevani@yahoo.com>
2025-02-01 22:09:14 -0500

    Fixed image optimization issue

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export", // ✅ ეს არის მნიშვნელოვანი Netlify-სთვის
+  images: {
+    unoptimized: true, // ✅ ეს გამორთავს Image Optimization API-ს
+  },
 };
 
 export default nextConfig;
